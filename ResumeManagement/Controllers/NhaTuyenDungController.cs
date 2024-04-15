971e95807f7d23be402e260b6afe2a1e4023ab8e
Duc Ha <haminhduc912003@gmail.com>
2024-04-15 18:14:58 +0700

    Add project files.

---
+++ ResumeManagement/Controllers/NhaTuyenDungController.cs
@@ -0,0 +1,92 @@
+﻿using ResumeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResumeManagement.Controllers
+{
+    public class NhaTuyenDungController : Controller
+    {
+        // GET: NhaTuyenDung
+        MyDataDataContext data = new MyDataDataContext();
+
+        // Action Index show danh sach nha vien
+
+        public ActionResult Index()
+        {
+            var all_ct = from s in data.CongTies select s;
+            return View(all_ct);
+        }
+        //------------Detail-------------------------------
+        public ActionResult Details(int id)
+        {
+            var D_ct = data.CongTies.Where(m => m.MaCongTy == id).First();
+            return View(D_ct);
+        }
+        //------------Create-------------------------------
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, CongTy s)
+        {
+            var tenct = collection["tenct"];
+            if (string.IsNullOrEmpty(tenct))
+            {
+                ViewData["Error"] = "Don't empty";
+                return View();
+            }
+            else
+            {
+                s.Ten = tenct;
+                data.CongTies.InsertOnSubmit(s); // Sửa thành InsertOnSubmit(s)
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+        }
+        //------------Edit-------------------------------//
+        public ActionResult Edit(int id)
+        {
+            var E_ct = data.CongTies.First(m => m.MaCongTy == id);
+            return View(E_ct);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var ct = data.CongTies.First(m => m.MaCongTy == id);
+            var E_tenct = collection["tenct"];
+            ct.MaCongTy = id;
+            if (string.IsNullOrEmpty(E_tenct))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                ct.Ten = E_tenct;
+                UpdateModel(ct);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Edit(id);
+        }
+        //------------Delete-------------------------------
+        //-------------Delete-------------------
+        public ActionResult Delete(int id)
+        {
+            var D_ct = data.CongTies.First(m => m.MaCongTy == id);
+            return View(D_ct);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            var D_ct = data.CongTies.Where(m => m.MaCongTy == id).First();
+            data.CongTies.DeleteOnSubmit(D_ct);
+            data.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ResumeManagement/Controllers/NhanVienController.cs
@@ -0,0 +1,93 @@
+﻿using ResumeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResumeManagement.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+
+        // Action Index show danh sach nha vien
+
+        public ActionResult Index()
+        {
+            // lấy ra tất cả các thể loại của sách
+            var all_nv = from s in data.NhanViens select s;
+            return View(all_nv);
+        }
+        //------------Detail-------------------------------
+        public ActionResult Details(int id)
+        {
+            var D_nv = data.NhanViens.Where(m => m.MaNhanVien == id).First();
+            return View(D_nv);
+        }
+        //------------Create-------------------------------
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, NhanVien s)
+        {
+            var tennv = collection["tennv"];
+            if (string.IsNullOrEmpty(tennv))
+            {
+                ViewData["Error"] = "Don't empty";
+                return View();
+            }
+            else
+            {
+                s.Ten = tennv;
+                data.NhanViens.InsertOnSubmit(s); // Sửa thành InsertOnSubmit(s)
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+        }
+        //------------Edit-------------------------------//
+        public ActionResult Edit(int id)
+        {
+            var E_nv = data.NhanViens.First(m => m.MaNhanVien == id);
+            return View(E_nv);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var nv = data.NhanViens.First(m => m.MaNhanVien == id);
+            var E_tennv = collection["tennv"];
+            nv.MaNhanVien = id;
+            if (string.IsNullOrEmpty(E_tennv))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                nv.Ten = E_tennv;
+                UpdateModel(nv);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Edit(id);
+        }
+        //------------Delete-------------------------------
+        //-------------Delete-------------------
+        public ActionResult Delete(int id)
+        {
+            var D_nv = data.NhanViens.First(m => m.MaNhanVien == id);
+            return View(D_nv);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            var D_nv = data.NhanViens.Where(m => m.MaNhanVien == id).First();
+            data.NhanViens.DeleteOnSubmit(D_nv);
+            data.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
